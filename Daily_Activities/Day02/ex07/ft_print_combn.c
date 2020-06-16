5d2be41ba1f7d56cc0e9debe087e05b463d188af
DannMartinnez <danielf.martinez.q@gmail.com>
2020-06-15 23:58:44 -0500

    adding ex07 to Day02

---
+++ Daily_Activities/Day02/ex07/ft_print_combn.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danmarti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/11 17:33:08 by danmarti          #+#    #+#             */
+/*   Updated: 2019/07/11 23:17:44 by danmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1,&c,1);
+}
+
+void	ft_xd(int n, int i, int *a, int j)
+{
+	while (a[i] <= 9)
+	{
+		if (i < n - 1)
+		{
+			a[i + 1] = a[i] + 1;
+			ft_xd(n, i + 1, a, j);
+		}
+		if (i == n - 1)
+		{
+			while (a[i] <= 9)
+			{
+				if (a[n - 1] != n - 1)
+				{
+					ft_putchar(',');
+					ft_putchar(' ');
+				}
+				while (++j <= n - 1)
+				{
+					ft_putchar(a[j] + '0');
+				}
+				a[i] = a[i] + 1;
+				j = -1;
+			}
+		}
+		a[i] = a[i] + 1;
+	}
+}
+void	ft_print_combn(int n)
+{
+	int a[n];
+	int i;
+	int j;
+
+	j = -1;
+	a[0] = 0;
+	i = 0;
+	ft_xd(n,i,a,j);
+}
+
+int	main(void)
+{
+	ft_print_combn(2);
+	return (0);
+}
